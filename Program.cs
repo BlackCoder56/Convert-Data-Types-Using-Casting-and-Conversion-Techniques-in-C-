97b430fc0d2b848e924723fd301fa7eeffb6217d
BlackCoder56 <blackcoda56@gmail.com>
2025-06-14 21:34:23 +0300

    Code challenge two

--- Program.cs
+++ Program.cs
@@ -57,23 +57,43 @@
 //     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
 // Code challenge One
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-decimal total = 0m;
-string message = "";
+// decimal total = 0m;
+// string message = "";
 
-foreach (var value in values)
-{
-    decimal number; // stores the TryParse "out" value
-    if (decimal.TryParse(value, out number))
-    {
-        total += number;
-    }
-    else
-    {
-        message += value;
-    }
-}
+// foreach (var value in values)
+// {
+//     decimal number; // stores the TryParse "out" value
+//     if (decimal.TryParse(value, out number))
+//     {
+//         total += number;
+//     }
+//     else
+//     {
+//         message += value;
+//     }
+// }
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
+
+// Outputing math operations as specific number types
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just trancate)
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divi de value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
--- obj/Debug/net8.0/conversion.AssemblyInfo.cs
+++ obj/Debug/net8.0/conversion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conversion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0511367c015e225275d77a4234fbc408633a17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7f8a65557ed49f373f460197109b62376f601e")]
 [assembly: System.Reflection.AssemblyProductAttribute("conversion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conversion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
